3a66bfa046d3539e9e5bf8a63b869356df392a26
ISNTIC-2021 <boussif.said@ofppt-edu.ma>
2022-12-15 11:54:00 +0100

    update 	modified:   package-lock.json 	modified:   package.json 	new file:   src/pages/NotFound.jsx 	modified:   src/routes/Routers.js

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box,  Typography } from '@mui/material';
+
+
+export default function Error() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        backgroundColor: '#df2020',
+      }}
+    >
+      <Typography variant="h1" style={{ color: 'white' }}>
+        404
+      </Typography>
+      <Typography variant="h6" style={{ color: 'white' }}>
+        The page you’re looking for doesn’t exist.
+      </Typography>
+    </Box>
+  );
+}
